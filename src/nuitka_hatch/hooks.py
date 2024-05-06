2cacb90f9f7ca3c76f361eb206df3e4a5f68ca55
HSPK <whxway@whu.edu.cn>
2024-05-06 13:37:45 +0800

    /rename

--- src/nuitka_hatch/hooks.py
+++ src/nuitka_hatch/hooks.py
@@ -1,5 +1,5 @@
 from hatchling.plugin import hookimpl
-from hatch_nuitka.plugin import NuitkaBuildHook
+from nuitka_hatch.plugin import NuitkaBuildHook
 
 @hookimpl
 def hatch_register_build_hook():
